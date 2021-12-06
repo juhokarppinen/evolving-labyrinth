ee88cdcb567b44d48dfb7aaeaca14902ab1e8d11
Juho Karppinen <juho@juhokarppinen.fi>
2021-12-06 22:14:16 +0200

    Preparing for v7

---
+++ src/Evergreen/V7/Types.elm
@@ -0,0 +1,40 @@
+module Evergreen.V7.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Evergreen.V7.Tile
+import Url
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , message : String
+    , tiles : List Evergreen.V7.Tile.Tile
+    , freeTile : Evergreen.V7.Tile.Tile
+    }
+
+
+type alias BackendModel =
+    { message : String
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+    | Shuffle
+    | ShuffleTiles (List Evergreen.V7.Tile.Tile)
+    | RandomRotateTiles (List Evergreen.V7.Tile.Rotation)
+
+
+type ToBackend
+    = NoOpToBackend
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = NoOpToFrontend
